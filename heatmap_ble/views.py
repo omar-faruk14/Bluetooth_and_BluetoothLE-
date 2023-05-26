e94567410e92c89522b4769f19d08edd1096fd78
Omar Faruk <Omarfaruk885@gmail.com>
2023-05-26 23:41:09 +0600

    Succesfully Heatmap Create Continuously

--- heatmap_ble/views.py
+++ heatmap_ble/views.py
@@ -15,6 +15,7 @@ import numpy as np
 import pandas as pd
 
 
+heatmap_data = []
 def index(request):
     return HttpResponse("Hello, World!")
 
@@ -24,10 +25,83 @@ def calculate_distance(rssi):
     distance = 10 ** ((tx_power - rssi) / (10 * n))
     return distance
 
-# Bleak BLE Device Information Scan
-async def scan_ble_devices(loop_count, sleep_duration):
+def create_heatmap(ble_devices):
+    if not ble_devices:
+        return None
+    df = pd.DataFrame(ble_devices)
+    if 'timestamp' not in df.columns:
+        return None
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    pivot_table = df.pivot_table(index='address', columns='timestamp', values='rssi', fill_value=0)
+    plt.figure(figsize=(10, 8))
+    cmap = sns.color_palette("viridis", as_cmap=True)
+    sns.heatmap(pivot_table, cmap=cmap, annot=True, fmt=".0f", cbar=True, linewidths=2)
+    plt.xlabel('Timestamp')
+    plt.ylabel('Address')
+    plt.title('RSSI Heatmap')
+    xticks_pos = np.arange(len(pivot_table.columns)) + 0.5
+    xticks_labels = pivot_table.columns.strftime("%H:%M:%S")
+    plt.xticks(xticks_pos, xticks_labels, rotation=45, ha='right')
+    yticks_pos = np.arange(len(pivot_table.index)) + 0.5
+    yticks_labels = pivot_table.index
+    plt.yticks(yticks_pos, yticks_labels)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    image_base64 = base64.b64encode(buf.read()).decode('utf-8').replace('\n', '')
+    buf.close()
+    return image_base64
+
+
+def create_heatmap2(ble_devices):
+    if not ble_devices:
+        return None
+    df = pd.DataFrame(ble_devices)
+    if 'timestamp' not in df.columns:
+        return None
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    pivot_table = df.pivot_table(index='address', columns='timestamp', values='rssi', fill_value=0)
+    plt.figure(figsize=(10, 8))
+    cmap = sns.color_palette("viridis", as_cmap=True)
+    sns.heatmap(pivot_table, cmap=cmap, annot=True, fmt=".0f", cbar=True, linewidths=2)
+    plt.xlabel('Timestamp')
+    plt.ylabel('Address')
+    plt.title('RSSI Heatmap')
+    xticks_pos = np.arange(len(pivot_table.columns)) + 0.5
+    xticks_labels = pivot_table.columns.strftime("%H:%M:%S")
+    plt.xticks(xticks_pos, xticks_labels, rotation=45, ha='right')
+    yticks_pos = np.arange(len(pivot_table.index)) + 0.5
+    yticks_labels = pivot_table.index
+    plt.yticks(yticks_pos, yticks_labels)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    image_base64 = base64.b64encode(buf.read()).decode('utf-8').replace('\n', '')
+    buf.close()
+    return image_base64
+
+
+async def add_heatmap(request):
+    ble_devices = await scan_ble_devices()
+    if ble_devices:
+        ble_devices = ble_devices
+        heatmap_image = create_heatmap(ble_devices)
+        if heatmap_image:
+            heatmap_data.append(heatmap_image)
+            return HttpResponse("Heatmap added successfully.")
+        else:
+            return HttpResponse("Invalid data format.")
+    else:
+        return HttpResponse("Invalid request method.")
+
+def display_heatmaps(request):
+    return render(request, 'display_heatmaps.html', {'heatmap_data': heatmap_data})
+
+async def scan_ble_devices():
+    loop_count=5
+    sleep_duration=1
     ble_devices = []
-    for loop_num in range(1, loop_count + 1):  # Run the loop five times
+    for loop_num in range(1, loop_count + 1):  
         devices = await BleakScanner.discover()
         loop_ble_devices = []
         for device in devices:
@@ -39,66 +113,21 @@ async def scan_ble_devices(loop_count, sleep_duration):
             }
             loop_ble_devices.append(ble_device)
             ble_devices.append(ble_device)
-
-        # Save loop_ble_devices to a JSON file
         filename = f'ble_devices_loop_{loop_num}.json'
         with open(filename, 'w') as file:
             json.dump(loop_ble_devices, file)
-
-        time.sleep(sleep_duration)  # Wait for 5 seconds before the next loop
-
-    # Save all ble_devices to a JSON file
+        time.sleep(sleep_duration)  
+    
     with open('ble_devices.json', 'w') as file:
         json.dump(ble_devices, file)
-
     return ble_devices
 
 
 def settings(request):
     return render(request, 'settings.html')
 
-def create_heatmap(ble_devices):
-    if not ble_devices:
-        return None  
-    df = pd.DataFrame(ble_devices)
-    if 'timestamp' not in df.columns:
-        return None
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-
-    # Create a pivot table with addresses as rows, timestamps as columns, and RSSI values as values
-    pivot_table = df.pivot_table(index='address', columns='timestamp', values='rssi', fill_value=0)
-
-    plt.figure(figsize=(10, 8))
-    cmap = sns.color_palette("viridis", as_cmap=True)
-    sns.heatmap(pivot_table, cmap=cmap, annot=True, fmt=".0f", cbar=True,linewidths=2)
-
-    plt.xlabel('Timestamp')
-    plt.ylabel('Address')
-    plt.title('RSSI Heatmap')
-    xticks_pos = np.arange(len(pivot_table.columns)) + 0.5
-    xticks_labels = pivot_table.columns.strftime("%H:%M:%S")
-    plt.xticks(xticks_pos, xticks_labels, rotation=45, ha='right')
-    yticks_pos = np.arange(len(pivot_table.index)) + 0.5
-    yticks_labels = pivot_table.index
-    plt.yticks(yticks_pos, yticks_labels)
-
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-    image_base64 = base64.b64encode(buf.read()).decode('utf-8').replace('\n', '')
-    buf.close()
-
-    return image_base64
-
 async def ble_devices_finder(request):
-    if request.method == 'POST':
-        loop_count = int(request.POST['loop_count'])
-        sleep_duration = int(request.POST['sleep_duration'])
-        ble_devices = await scan_ble_devices(loop_count, sleep_duration)
-        heatmap_image = create_heatmap(ble_devices)
-
-    else:
-        return HttpResponse("Invalid request method.")
-
+    ble_devices = await scan_ble_devices()
+    heatmap_image = create_heatmap(ble_devices)
     return render(request, 'heatmap.html', {'heatmap_image': heatmap_image,'ble_devices':ble_devices})
 
