226e43bded4fb38624509a88fd6cce6a7a39f345
Omar Faruk <Omarfaruk885@gmail.com>
2023-05-28 01:46:24 +0600

    Timestamp Add

--- heatmap_ble/views.py
+++ heatmap_ble/views.py
@@ -50,52 +50,38 @@ def create_heatmap(ble_devices):
     buf.seek(0)
     image_base64 = base64.b64encode(buf.read()).decode('utf-8').replace('\n', '')
     buf.close()
-    return image_base64
+    
+    heatmap = {
+        'timestamp': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'image': image_base64
+    }
+    return heatmap
 
-
-def create_heatmap2(ble_devices):
-    if not ble_devices:
-        return None
-    df = pd.DataFrame(ble_devices)
-    if 'timestamp' not in df.columns:
-        return None
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    pivot_table = df.pivot_table(index='address', columns='timestamp', values='rssi', fill_value=0)
-    plt.figure(figsize=(10, 8))
-    cmap = sns.color_palette("viridis", as_cmap=True)
-    sns.heatmap(pivot_table, cmap=cmap, annot=True, fmt=".0f", cbar=True, linewidths=2)
-    plt.xlabel('Timestamp')
-    plt.ylabel('Address')
-    plt.title('RSSI Heatmap')
-    xticks_pos = np.arange(len(pivot_table.columns)) + 0.5
-    xticks_labels = pivot_table.columns.strftime("%H:%M:%S")
-    plt.xticks(xticks_pos, xticks_labels, rotation=45, ha='right')
-    yticks_pos = np.arange(len(pivot_table.index)) + 0.5
-    yticks_labels = pivot_table.index
-    plt.yticks(yticks_pos, yticks_labels)
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-    image_base64 = base64.b64encode(buf.read()).decode('utf-8').replace('\n', '')
-    buf.close()
-    return image_base64
 
 
 async def add_heatmap(request):
     ble_devices = await scan_ble_devices()
     if ble_devices:
-        ble_devices = ble_devices
-        heatmap_image = create_heatmap(ble_devices)
-        if heatmap_image:
-            heatmap_data.append(heatmap_image)
+        heatmap = create_heatmap(ble_devices)
+        if heatmap:
+            heatmap_data.append(heatmap)
             return HttpResponse("Heatmap added successfully.")
         else:
             return HttpResponse("Invalid data format.")
     else:
         return HttpResponse("Invalid request method.")
 
+
+
 def display_heatmaps(request):
-    return render(request, 'display_heatmaps.html', {'heatmap_data': heatmap_data})
+    heatmap_data_with_timestamp = []
+    for heatmap in heatmap_data:
+        timestamp = heatmap['timestamp']
+        image = heatmap['image']
+        heatmap_with_timestamp = {'timestamp': timestamp, 'image': image}
+        heatmap_data_with_timestamp.append(heatmap_with_timestamp)
+    return render(request, 'display_heatmaps.html', {'heatmap_data': heatmap_data_with_timestamp})
+
 
 async def scan_ble_devices():
     loop_count=5
